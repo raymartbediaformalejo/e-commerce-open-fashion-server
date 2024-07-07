f206033efd1d87ee369db0204a853e967301f1c8
Raymart <100412726+raymartbediaformalejo@users.noreply.github.com>
2024-07-07 18:40:10 +0800

    add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
 import productsRoutes from "./products/products.routes.js";
@@ -8,6 +9,7 @@ const app = express();
 app.listen(PORT, () =>
   console.log(`Server is now running at http://localhost:${PORT}`)
 );
+app.use(cors({ origin: ["http://localhost:3000", ""], credentials: true }));
 app.use(express.json());
 
 app.use("/api", productsRoutes);
