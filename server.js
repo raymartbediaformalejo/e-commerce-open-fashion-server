75aa74addd8d4ca6acba509ee68ab96b9d0a4331
Raymart <100412726+raymartbediaformalejo@users.noreply.github.com>
2024-07-10 16:36:36 +0800

    feat: add link to cors origin

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const app = express();
 app.listen(PORT, () =>
   console.log(`Server is now running at http://localhost:${PORT}`)
 );
-app.use(cors({ origin: ["http://localhost:3000", ""], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://ecommerce-six-blond-87.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api", productsRoutes);
